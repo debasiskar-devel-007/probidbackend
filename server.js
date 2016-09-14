dce0b2e62a1d911b61a94c4ab4f5e2655e8bf86c
debasiskar-devel-007 <debasiskar007@gmail.com>
2016-09-14 19:23:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,137 @@
+/**
+ * Created by debasis on 14/9/16.
+ */
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8001;
+
+
+var http = require('http').Server(app);
+
+
+
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({ parameterLimit: 10000000,
+    limit: '90mb'}));
+app.use(bodyParser.urlencoded({ parameterLimit: 10000000,
+    limit: '90mb', extended: false}));
+var multer  = require('multer');
+var datetimestamp='';
+var filename='';
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+
+        console.log(file.originalname);
+        filename=file.originalname.split('.')[0].replace(/ /g,'') + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1];
+        console.log(filename);
+        cb(null, filename);
+    }
+});
+
+
+var EventEmitter = require('events').EventEmitter;
+
+const emitter = new EventEmitter()
+//emitter.setMaxListeners(100)
+// or 0 to turn off the limit
+emitter.setMaxListeners(0)
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('file');
+
+
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+
+
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
+
+/** API path that will upload the files */
+app.post('/uploads', function(req, res) {
+    datetimestamp = Date.now();
+    upload(req,res,function(err){
+
+        if(err){
+            res.json({error_code:1,err_desc:err});
+            return;
+        }
+
+
+        res.json({error_code:0,filename:filename});
+    });
+});
+
+var mongodb = require('mongodb');
+var db;
+var url = 'mongodb://localhost:27017/probidbackend';
+
+var MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(url, function (err, database) {
+    if (err) {
+        console.log(err);
+
+    }else{
+        db=database;
+
+    }});
+
+
+
+
+app.get('/addexpertarea', function (req, resp) {
+
+    value1 = {title: 'sdf',description: '5435', priority: 6,status: 0};
+
+    var collection = db.collection('addexpertarea');
+
+    collection.insert([value1], function (err, result) {
+        if (err) {
+            resp.send(err);
+        } else {
+            resp.send('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+
+        }
+    });
+
+});
+
+
+app.get('/listexpert', function (req, resp) {
+
+
+    var collection = db.collection('addexpertarea');
+
+    collection.find().toArray(function(err, items) {
+
+        resp.send(JSON.stringify(items));
+    });
+
+});
+
+
+
+var server = app.listen(port, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    //  console.log("Example app listening at http://%s:%s", host, port)
+
+})
